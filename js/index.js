64a5e852e57e62caf7b7048b5d5762519ff55e12
workeazee <workeazee@gmail.com>
2022-02-03 12:13:32 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  $(".tabs").tabs();
+  $('.tooltipped').tooltip();
+
+  $('.tooltipped').on('click mouseover', function() {
+    $(this).tooltip();
+    $(this).tooltip('open');
+  });
+      
+  $('.tooltipped').mouseleave(function() {
+    if ($(this).tooltip()){
+      $(this).tooltip('destroy');
+    }
+  });
+
+
+});
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 0) {
+    $("nav").addClass("scrolled");
+    $("nav").addClass("sticky-nav");
+  } else {
+    $("nav").removeClass("scrolled");
+    $("nav").removeClass("sticky-nav");
+  }
+});
